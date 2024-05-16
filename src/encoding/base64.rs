c1ef54a9bec31a9539b74298bad01e1495fc2f2a
Hoang Quoc Trung <trung.hoang@pricehubble.com>
2024-05-16 17:05:26 +0200

    Fix base64 encoding
    Signed-off-by: Hoang Quoc Trung <trung.hoang@pricehubble.com>

--- src/encoding/base64.rs
+++ src/encoding/base64.rs
@@ -37,32 +37,22 @@ pub fn decode(input: &str) -> String {
 
     for window in input.trim_end_matches('=').as_bytes().chunks(4) {
         let n = window.len();
-        let mut decoded = [0u8; 3];
-
         let indexes: Vec<u8> = window.iter().map(|x| inverse(*x as char)).collect();
         match n {
-            1 => {
-                decoded[0] = (indexes[0] & 0x7C) << 2;
-            }
             2 => {
-                decoded[0] = ((indexes[0] & 0x7C) << 2) | ((indexes[1] & 0x30) >> 4);
-                decoded[1] = (indexes[1] & 0xF) << 4;
+                res.push(((indexes[0] << 2) | ((indexes[1] & 0x30) >> 4)) as char);
             }
             3 => {
-                decoded[0] = ((indexes[0] & 0x7C) << 2) | ((indexes[1] & 0x30) >> 4);
-                decoded[1] = ((indexes[1] & 0xF) << 4) | ((indexes[2] & 0x3C) >> 2);
+                res.push(((indexes[0] << 2) | ((indexes[1] & 0x30) >> 4)) as char);
+                res.push((((indexes[1] & 0xF) << 4) | ((indexes[2] & 0x3C) >> 2)) as char);
             }
             4 => {
-                decoded[0] = ((indexes[0] & 0x7C) << 2) | ((indexes[1] & 0x30) >> 4);
-                decoded[1] = ((indexes[1] & 0xF) << 4) | ((indexes[2] & 0x3C) >> 2);
-                decoded[2] = ((indexes[2]) << 6) | (indexes[3] & 0x3F);
+                res.push(((indexes[0] << 2) | ((indexes[1] & 0x30) >> 4)) as char);
+                res.push((((indexes[1] & 0xF) << 4) | ((indexes[2] & 0x3C) >> 2)) as char);
+                res.push((((indexes[2]) << 6) | (indexes[3] & 0x3F)) as char);
             }
             _ => {}
         }
-
-        res.push(decoded[0] as char);
-        res.push(decoded[1] as char);
-        res.push(decoded[2] as char);
     }
     res
 }
@@ -79,16 +69,16 @@ pub fn encode(input: &str) -> String {
         match n {
             1 => {
                 encoded[0] = lookup(window[0] >> 2);
-                encoded[1] = lookup((window[0] & 0xC0) << 4);
+                encoded[1] = lookup((window[0] & 0x03) << 4);
             }
             2 => {
                 encoded[0] = lookup(window[0] >> 2);
-                encoded[1] = lookup(((window[0] & 0x3F) << 4) | (window[1] >> 4));
+                encoded[1] = lookup(((window[0] & 0x03) << 4) | (window[1] >> 4));
                 encoded[2] = lookup((window[1] & 0xF) << 2);
             }
             3 => {
                 encoded[0] = lookup(window[0] >> 2);
-                encoded[1] = lookup(((window[0] & 0x3F) << 4) | (window[1] >> 4));
+                encoded[1] = lookup(((window[0] & 0x03) << 4) | (window[1] >> 4));
                 encoded[2] = lookup(((window[1] & 0xF) << 2) | (window[2] >> 6));
                 encoded[3] = lookup(window[2] & 0x3F);
             }
@@ -109,19 +99,33 @@ mod tests {
     use rstest::*;
 
     #[rstest]
-    #[case("a", "YA==")]
+    #[case("f", "Zg==")]
+    #[case("fo", "Zm8=")]
+    #[case("foo", "Zm9v")]
+    #[case("foob", "Zm9vYg==")]
+    #[case("fooba", "Zm9vYmE=")]
+    #[case("foobar", "Zm9vYmFy")]
+    #[case("a", "YQ==")]
     #[case("Su", "U3U=")]
     #[case("Sun", "U3Vu")]
     #[case("Sund", "U3VuZA==")]
     fn test_encoding(#[case] input: &str, #[case] expected: &str) {
-        assert_eq!(encode(input), expected.to_string());
+        let encoded = encode(input);
+        assert_eq!(encoded, expected.to_string());
+        
+        assert_eq!(decode(&encoded), input);
     }
 
     #[rstest]
-    #[case("YQ==")]
-    #[case("U3U=")]
-    #[case("U3Vu")]
-    #[case("U3VuZA==")]
+    // #[case("Uw==")]
+    // #[case("U3U=")]
+    // #[case("U3Vu")]
+    // #[case("ZA==")]
+    #[case("ZGE=")]
+    // #[case("U3VuZA==")]
+    // #[case("Zm8=")]  
+    // #[case("Zm9v")]
+    // #[case("YQ==")]
     fn test_decoding(#[case] input: &str) {
         println!("{}", decode(input));
     }
