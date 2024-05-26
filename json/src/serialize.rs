500cd530358524c9d95ace30f17e15b8e89b8b24
Hoang Quoc Trung <trung.hoang@pricehubble.com>
2024-05-26 14:05:29 +0200

    WIP:
    Signed-off-by: Hoang Quoc Trung <trung.hoang@pricehubble.com>

---
+++ json/src/serialize.rs
@@ -0,0 +1,4 @@
+
+pub trait Serialize {
+    fn holla() -> bool;
+}
---
+++ json_derive/src/lib.rs
@@ -0,0 +1,28 @@
+#[macro_use]
+extern crate quote;
+#[macro_use]
+extern crate syn;
+
+extern crate proc_macro;
+
+use proc_macro::TokenStream;
+use syn::DeriveInput;
+
+#[proc_macro_derive(Serialize)]
+pub fn my_macro(input: TokenStream) -> TokenStream {
+    // Parse the input tokens into a syntax tree
+    let input = parse_macro_input!(input as DeriveInput);
+
+    match input.data {
+        Data::Struct(ref data) => {}
+        Data::Enum(ref data) => {}
+        Data::Union(_) => {} 
+    }
+    // Build the output, possibly using quasi-quotation
+    let expanded = quote! {
+        // ...
+    };
+
+    // Hand the output tokens back to the compiler
+    TokenStream::from(expanded)
+}
