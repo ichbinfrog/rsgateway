19500674a9621b79214eb706ae38b0dbd0ba7ea6
Hoang Quoc Trung <quoctrunghoang1998@gmail.com>
2024-06-07 22:00:46 +0200

    First draft of udp packet
    Signed-off-by: Hoang Quoc Trung <quoctrunghoang1998@gmail.com>

--- udp/src/lib.rs
+++ udp/src/lib.rs
@@ -1,25 +1,42 @@
 use bitarray::{
     buffer::{self, SizedString},
-    serialize::{self, Serialize},
+    serialize::{self, Deserialize, Serialize},
 };
-use bitarray_derive::Serialize;
+use bitarray_derive::{Deserialize, Serialize};
 
 // A TCP Header as defined in [RFC-9293](https://datatracker.ietf.org/doc/html/rfc9293#section-3.1)
-#[derive(Serialize, PartialEq, Debug)]
+#[derive(Serialize, Deserialize, PartialEq, Debug)]
 pub struct Header<const N: usize> {
     src: u16,
     dst: u16,
     length: u16,
     checksum: u16,
-    data: SizedString<N>,
+
+    #[rustfmt::skip]
+    data: SizedString::<N>,
 }
 
 #[cfg(test)]
 pub mod tests {
+    use buffer::Buffer;
+
     use super::*;
 
     #[test]
     fn test_udp_serialization() {
-        
+        let header = Header::<2> {
+            src: 1,
+            dst: 2,
+            checksum: 3,
+            length: 32,
+            data: SizedString("holla".to_string()),
+        };
+        let mut buf = Buffer::new(252);
+        let n = header.serialize(&mut buf).unwrap();
+        buf.reset();
+
+        let (res, m) = Header::<2>::deserialize(&mut buf).unwrap();
+        assert_eq!(n, m);
+        assert_eq!(header, res);
     }
 }
